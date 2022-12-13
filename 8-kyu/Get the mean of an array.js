7a62eda9fcdf606e45ef37a156a70a3dac44d235
Yauheni Haurylenka <57916966+Broiller@users.noreply.github.com>
2022-12-14 01:40:49 +0300

    Create Get the mean of an array.js

---
+++ 8-kyu/Get the mean of an array.js
@@ -0,0 +1,16 @@
+/*
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+*/
+function getAverage(marks){
+  console.log(marks);
+  let sum = 0;
+  for (let i = 0; i < marks.length; i++){
+    sum = sum + marks[i];
+  }
+  return Math.floor(sum/marks.length);
+  //TODO : calculate the downward rounded average of the marks array
+}
