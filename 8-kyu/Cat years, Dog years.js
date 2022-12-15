9ace873362e35d249790c1938faf1be5b099f622
Yauheni Haurylenka <57916966+Broiller@users.noreply.github.com>
2022-12-16 00:16:57 +0300

    Create Cat years, Dog years.js

---
+++ 8-kyu/Cat years, Dog years.js
@@ -0,0 +1,46 @@
+/*
+I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+References
+
+http://www.catster.com/cats-101/calculate-cat-age-in-cat-years
+http://www.slate.com/articles/news_and_politics/explainer/2009/05/a_dogs_life.html
+*/
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let hY = 0;
+  let kY = 0;
+  let dY = 0;
+  if(humanYears === 1) {
+  let hY = humanYears;
+  let kY = humanYears*15;
+  let dY = humanYears*15;
+    return [hY,kY,dY];
+  } else if(humanYears === 2) {
+  let hY = humanYears;
+  let kY = (humanYears-1)*15+(humanYears-1)*9;
+  let dY = (humanYears-1)*15+(humanYears-1)*9;
+    return [hY,kY,dY];
+  } else {
+  let hY = humanYears;
+  let kY = 24+(humanYears-2)*4;
+  let dY = 24+(humanYears-2)*5;
+    return [hY,kY,dY];
+  }
+}
