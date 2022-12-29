d7f108e463207713010bb112e053cb1a0e6c9c43
Yauheni Haurylenka <57916966+Broiller@users.noreply.github.com>
2022-12-30 02:54:02 +0300

    Create Create Check same case.js

---
+++ 8-kyu/Create Check same case.js
@@ -0,0 +1,22 @@
+/*
+Write a function that will check if two given characters are the same case.
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+Examples
+'a' and 'g' returns 1
+'A' and 'C' returns 1
+'b' and 'G' returns 0
+'B' and 'g' returns 0
+'0' and '?' returns -1
+*/
+
+function sameCase(a, b){
+  if(a.toLowerCase() == a.toUpperCase() || b.toLowerCase() == b.toUpperCase()) {
+    return -1
+  } if(a.toLowerCase() == a && b.toLowerCase() == b || a.toUpperCase() == a && b.toUpperCase() == b) {
+    return 1
+  } else {
+    return 0
+  }
+}
