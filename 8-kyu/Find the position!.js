584ccae10dddc9faded4fa5b842210882382f6a7
Yauheni Haurylenka <57916966+Broiller@users.noreply.github.com>
2023-01-01 14:06:54 +0300

    Create Find the position!.js

---
+++ 8-kyu/Find the position!.js
@@ -0,0 +1,12 @@
+/*
+Description:
+When provided with a letter, return its position in the alphabet.
+Input :: "a"
+Ouput :: "Position of alphabet: 1"
+This kata is meant for beginners. Rank and upvote to bring it out of beta
+*/
+
+function position(letter){
+  console.log(letter.charCodeAt()-96)
+  return "Position of alphabet: " + (letter.charCodeAt()-96)
+}
